89eee6ab7f6227c245fd282509c8f78a5a84368f
Tony Chan <tony.chan@vonage.com>
2026-09-15 14:58:55 -0700

    initial

---
+++ README.md
@@ -0,0 +1,171 @@
+# Vonage API WebRTC Client call to Vonage Contact Center (VCC) 
+
+This repository includes a sample WebRTC Client code in JavaScript using Vonage Client SDK, as well as the corresponding sample server code allowing the WebRTC client to make a call to VCC.
+
+Using a WebRTC client is also known as in-app voice.
+
+The sample code here allows a WebRTC client to make calls to VCC, to receive calls from PSTN, to make calls to PSTN, make or receive calls with another WebRTC client.
+
+## Set up
+
+### Local deployment using ngrok
+
+If you plan to test using `Local deployment with ngrok` (Internet tunneling service), here are the instructions to set up ngrok:<br>
+- [Install ngrok](https://ngrok.com/download),<br>
+- Make sure you are using the latest version of ngrok and not using a previously installed version of ngrok,
+- Sign up for a free [ngrok account](https://dashboard.ngrok.com/signup),<br>
+- Verify your email address from the email sent by ngrok,<br>
+- Retrieve [your Authoken](https://dashboard.ngrok.com/get-started/your-authtoken),<br>
+- Run the command `ngrok config add-authtoken <your-authtoken>`<br>
+- Set up the tunnel
+	- Run `ngrok config edit`
+		- For a free ngrok account, add following lines to the ngrok configuration file (under authoken line):</br>
+		<pre><code>	
+		tunnels:
+			mytunnel:</br>
+				proto: http</br>
+				addr: 8000</br>
+		</code></pre>
+		- For a [paid ngrok account](https://dashboard.ngrok.com/billing/subscription), you may set a ngrok hostname that never changes on each ngrok new launch, add following lines to the ngrok configuration file (under authoken line) - set hostname to actual desired values:</br>
+		<pre><code>	
+		tunnels:
+			mytunnel:</br>
+				proto: http</br>
+				addr: 8000</br>
+				hostname: setahostnamehere.ngrok.io</br>
+		</code></pre>			
+		
+- Start the ngrok tunnel
+	- Run `ngrok start mytunnel`</br>
+	- You will see lines like
+		....</br>
+		*Web Interface                 http://127.0.0.1:4040</br>                             
+		Forwarding                    https://xxxxxx.ngrok.io -> http://localhost:8000*</br> 
+	- Make note of *https://xxxxxx.ngrok.io* (with the leading https://), as it will be needed in the next steps below.</br>	
+
+Reminder: The Node.js server application (this repository) is running on local port 8000.</br>
+
+### Non local deployment
+
+If you are using hosted servers, for example VCR (Vonage Cloud Runtime), Heroku, your own servers, or some other cloud provider,
+you will need the public hostname and if necessary public port of the server that
+run this server application (from this repository), e.g.</br>
+	*`myappname.herokuapp.com`, `myserver.mycompany.com:40000`*</br>
+
+For Heroku deployment, see more details in the next section **Command Line Heroku deployment**.
+
+### Set up your Vonage Voice API application credentials and phone number
+
+[Log in to your](https://ui.idp.vonage.com/ui/auth/login) or [sign up for a](https://ui.idp.vonage.com/ui/auth/registration) Vonage API account.
+
+Go to [Your applications](https://dashboard.vonage.com/applications), access an existing application or [+ Create a new application](https://dashboard.vonage.com/applications/new).
+
+Under **Capabilities** section (click on [Edit] if you do not see this section):
+
+Enable Voice
+- Under Answer URL, leave HTTP GET, and enter https://\<host\>:\<port\>/voice/answer (replace \<host\> and \<port\> with the public host name and if necessary public port of the server where this sample application is running), e.g.</br>
+*https://yyyyyyyy.ngrok.io/voice/answer*</br>
+or
+*https://myappname.herokuapp.com/voice/answer*</br>
+or
+*https://myserver2.mycompany.com:40000/voice/answer*</br>
+- Under Event URL, **select** **_HTTP POST_**, and enter https://\<host\>:\<port\>/voice/event (replace \<host\> and \<port\> with the public host name and if necessary public port of the server where this sample application is running), e.g.</br>
+*https://yyyyyyyy.ngrok.io/voice/event*</br>
+or
+*https://myappname.herokuapp.com/voice/event*</br>
+or
+*https://myserver2.mycompany.com:40000/voice/event*</br>
+
+- Click on [Generate public and private key] if you did not yet create or want new ones, then save as **.private.key** file (note the leading dot in the file name) in this application folder.</br>
+**IMPORTANT**: Do not forget to click on [Save changes] at the bottom of the screen if you have created a new key set.</br>
+- Link a phone number to this application if none has been linked to the application.
+
+Please take note of your **application ID** and a **linked phone number** (as they are needed in the very next section.)
+
+Note: You may have more than one phone number linked to your application if needed.
+
+For the next steps, you will need:</br>
+- Your [Vonage API key](https://dashboard.vonage.com/settings) (as **`API_KEY`**)</br>
+- Your Vonage API secret, not signature secret, (as **`API_SECRET`**)</br>
+- Your `application ID` (as **`APP_ID`**),</br>
+- Optionally, a **`phone number linked`** to your application (as **`SERVICE_NUMBER`**).</br>
+
+### Set up the server application
+
+Copy or rename env-example to .env<br>
+Update parameters in .env file<br>
+Have Node.js installed on your system, this application has been tested with Node.js version 22.16<br>
+Install node modules with the command "npm install"<br>
+Start the server application with the command "node voice-api-to-vcc"<br>
+This Node.js server application (this repository) is running on local port 8000.</br>
+
+### Command Line Heroku deployment
+
+You must first have deployed your application locally, as explained in previous section, and verified it is working.
+
+Install [git](https://git-scm.com/downloads).
+
+Install [Heroku command line](https://devcenter.heroku.com/categories/command-line) and login to your Heroku account.
+
+If you do not yet have a local git repository, create one:</br>
+```bash
+git init
+git add .
+git commit -am "initial"
+```
+
+Start by creating this application on Heroku from the command line using the Heroku CLI:
+*Note: In following command, replace "myappname" with a unique name on the whole Heroku platform*
+
+```bash
+heroku create myappname
+```
+
+On your Heroku dashboard where your application page is shown, click on `Settings` button,
+add the following `Config Vars` and set them with their respective values:</br>
+API_KEY</br>
+API_SECRET</br>
+APP_ID</br>
+SERVICE_NUMBER</br>
+API_REGION</br>
+CLIENT_SDK_USERS</br>
+
+Now, deploy the application:
+
+
+```bash
+git push heroku master
+```
+
+On your Heroku dashboard where your application page is shown, click on `Open App` button, that hostname is the one to be used under your corresponding [Vonage Voice API application Capabilities](https://dashboard.nexmo.com/applications) (click on your application, then [Edit]).</br>
+
+For example, the respective links would be (replace *myappname* with actual value):</br>
+https://myappname.herokuapp.com/voice/answer</br>
+https://myappname.herokuapp.com/voice/event</br>
+
+See more details in above section **Set up your Vonage Voice API application credentials and phone number**.
+
+
+## WebRTC client
+
+The sample WebRTC client in this repository is based on the Vonage Client SDK.</br>
+
+Open it in a web browser using the address:</br>
+
+https://myserver-address/client.html</br>
+
+e.g.</br>
+https://xxxxxx.ngrok.io/client.html</br>
+or</br>
+https://myappname.herokuapp.com/client.html</br>
+or</br>
+https://myserver.mycompany.com:40000/client.html</br>
+
+You are ready to:</br>
+- Receive PSTN calls,</br>
+- Make PSTN calls,</br>
+- Receive or place a call to/from another WebRTC client (opened from the same link - logged in as a different user).</br>
+
+
+
+
---
+++ voice-api-to-vcc.js
@@ -0,0 +1,480 @@
+'use strict'
+
+//-------------
+
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const fs = require('fs');
+
+const axios = require('axios');
+
+//-- CORS - update as needed for your environment -
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
+});
+
+//--
+
+app.use(bodyParser.json());
+
+//------------------------------
+
+const appId = process.env.APP_ID;
+const serviceNumber = process.env.SERVICE_NUMBER;
+const vccNumber = process.env.VCC_NUMBER;
+const vccRegion = process.env.VCC_REGION;
+const apiRegion = process.env.API_REGION;
+const dc = apiRegion.substring(4, 8);
+
+// ------------------
+
+console.log("Service phone number:", serviceNumber);
+
+console.log("VCC number:", vccNumber);
+console.log("VCC region:", vccRegion);
+
+//-------------------
+
+const { Auth } = require('@vonage/auth');
+
+const credentials = new Auth({
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+  applicationId: appId,
+  privateKey: './.private.key'
+});
+
+const options = {
+  apiHost: apiRegion
+};
+
+const { Vonage } = require('@vonage/server-sdk');
+
+// const vonage = new Vonage(credentials, options);
+const vonage = new Vonage(credentials);
+
+const privateKey = fs.readFileSync('./.private.key');
+
+const { tokenGenerate } = require('@vonage/jwt');
+
+//-- List of allowable client SDK (WebRTC client) users --
+
+const clientList = process.env.CLIENT_SDK_USERS.toLowerCase().split(/\s*,+\s*/);
+
+let clients = new Set();
+
+for (let client in clientList){
+  clients.add(clientList[client]);
+};
+
+console.log('List of allowable clients:', clients);
+
+// create IVR voice prompts for PSTN incoming calls, the following example assumes
+// you did specify more than 1 and up to 9 allowable client SDK user names (in .env file)
+
+// let ivrPrompt = "Welcome to our company. ";
+
+// let index = 0;
+// for (let client of clients) {
+//   index++;
+//   ivrPrompt = ivrPrompt + `To speak to ${client}, press ${index}. `;
+// };
+
+const clientsArray = Array.from(clients);
+
+// no IVR for direct in-app to in-app calls (WebRTC client to WebRTC client)
+
+//==========================================================
+
+app.get('/answer', (req, res) => {
+
+  const hostName = req.hostname;
+
+  let nccoResponse;
+
+  if (req.query.from_user) {  // is it a call from a client SDK (WebRTC client)?
+
+    if (/^\d+$/.test(req.query.to)) { // is the "to" field value all digits? if yes, call that PSTN number
+
+      if (req.query.to == serviceNumber) {  // a client SDK calling this number will get bridged with the VCC number
+
+        nccoResponse = [
+          { // this "talk" action may be skipped and just let VCC say initial voice prompts
+            "action": "talk",
+            "text": "Connecting your call, please wait.",
+            "language": "en-US",
+            "style": 0
+          },
+          {
+            "action": "conversation",
+            "name": `conf_${req.query.uuid}`, // create a unique named conference
+            "startOnEnter": true,
+            "endOnExit": true
+          }
+        ];
+
+      } else {  // call from a client SDK to an external phone number
+
+        nccoResponse = [
+          {
+            "action": "talk",
+            "text": "Connecting your call, please wait",
+            "language": "en-US",
+            "style": 0
+          },        
+          {
+            "action": "connect",
+            "eventUrl": [`https://${hostName}/inappevent`], 
+            "timeout": 45,
+            "from": serviceNumber,
+            "endpoint": [
+              {
+                "type": "phone",
+                "number": req.query.to
+              }
+            ]
+          }
+        ];
+
+      }  
+
+    } else {  // call from a client SDK to another client SDK
+
+      const calledUser = req.query.to.toLowerCase();
+
+      if (calledUser == 'support') {  // this client SDK call will be bridged to the call to VCC number
+
+        nccoResponse = [
+          { // this "talk" action may be skipped and just let VCC say initial voice prompts
+            "action": "talk",
+            "text": "Connecting your call, please wait.",
+            "language": "en-US",
+            "style": 0
+          },
+          {
+            "action": "conversation",
+            "name": `conf_${req.query.uuid}`, // create a unique named conference
+            "startOnEnter": true,
+            "endOnExit": true
+          }
+        ];
+
+      } else {  // calling another client SDK user
+
+        nccoResponse = [
+          {
+            "action": "talk",
+            "text": "Connecting your call, please wait",
+            "language": "en-US",
+            "style": 0
+          },
+          { 
+            "action": "connect", 
+            "from": req.query.from,
+            "endpoint": [ 
+              { "type": "app", 
+                "user": calledUser
+              }
+            ],
+            "timeout": 45,
+            "eventUrl": [`https://${hostName}/inappevent`], 
+            "eventMethod": "POST" 
+          }
+        ];
+
+      }  
+        
+    }  
+
+  } else {  // this incoming call from a non client SDK (WebRTC client) will get bridged with the call to VCC number
+
+    nccoResponse = [
+      {  // this "talk" action may be skipped and just let VCC say initial voice prompts
+        "action": "talk",
+        "text": "Connecting your call, please wait.",
+        "language": "en-US",
+        "style": 0
+      },
+      {
+        "action": "conversation",
+        "name": `conf_${req.query.uuid}`, // create a unique named conference
+        "startOnEnter": true,
+        "endOnExit": true
+      }
+    ];
+
+  };  
+
+  res.status(200).json(nccoResponse);
+
+});
+
+//--------
+
+app.post('/event', (req, res) => {
+
+  const uuid = req.body.uuid;
+  const hostName = req.hostname;
+
+  if (req.body.type == 'transfer') {  // this is when the incoming call is effectively connected to the named conference
+
+    //-- Create outbound call to VCC --
+
+    // console.log('>>> SIP URI:', `sip:${vccNumber}@${vccRegion}`);
+
+    vonage.voice.createOutboundCall({
+      to: [{
+        type: 'sip',
+        uri: `sip:${vccNumber}@${vccRegion}`,
+        standard_headers: {
+          "User-to-User": req.body.from_user // pass the caller info
+        }
+      }],
+      from: {
+       type: 'phone',
+       number: serviceNumber
+      },
+      // answer_url: ['https://' + hostName + '/answer_vcc?original_uuid=' + uuid],
+      // answer_method: 'GET',
+      ncco: [
+        {
+        "action": "conversation",
+        "name": `conf_${uuid}`, // use the same unique named conference when peer incoming call was answered
+        "startOnEnter": true,
+        "endOnExit": true
+        }
+      ],
+      event_url: ['https://' + hostName + '/event_vcc?original_uuid=' + uuid],
+      event_method: 'POST'
+      })
+      .then(res => console.log(">>> Outbound VCC call status:", res))
+      .catch(err => console.error(">>> Outbound VCC call error:", err))
+
+  }
+
+  res.status(200).send('Ok');
+  
+});
+
+//--------
+
+app.post('/event_vcc', (req, res) => {
+
+  res.status(200).send('Ok');
+  
+});
+
+//--------
+
+app.post('/inappevent', (req, res) => {
+
+  res.status(200).send('Ok');
+  
+});
+
+//--------
+
+// app.post('/rtc', (req, res) => {
+
+//   res.status(200).send('Ok');
+  
+// });
+
+//=== Services for the WebRTC client (Vonage client SDK) ===============
+
+app.post('/login', async (req, res) => {
+
+    const user = req.body.user; // web page should have already made the name to lower case
+
+    // check if user is in the list of allowable users
+    if (!clients.has(user)) {
+      return res.status(401).json({ name: user, message: ">>> Unknown user" });
+    }
+
+    console.log("Creating user: " + user);
+    // either get or create this user (if not yet existing)
+    const userId = await getUser(user);
+    
+    console.log("Generating JWT for user: " + user);
+    const jwt = await generateJWT(user);
+        
+    return res.status(200).json({ name: user, userId: userId, token: jwt, dc: dc, phone: serviceNumber });
+})
+
+//--------
+
+async function getUser(name) {
+    
+  const accessToken = tokenGenerate(appId, privateKey, {});
+  
+  return new Promise(async (resolve, reject) => {
+    
+    let results;
+    
+    try {
+      results = await axios.get('https://api.nexmo.com/v0.3/users?name=' + name,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
+        });
+
+      //-- debug
+      // console.log(">>> results.data:", results.data);
+      // console.log("User Retrieval results: ", results.data._embedded.users[0].id);
+      
+      // If user already exists, just use it!
+      resolve(results.data._embedded.users[0].id);
+      return;
+    } 
+    catch (err) {
+
+        console.log(">>> err.response:", err.response);
+        // console.log("User retrieval error: ", err.response.data)
+    }
+    
+    // Here - user does NOT exist, create it
+    try {
+        let body = {
+            name: name,
+            display_name: name
+        }
+        results = await axios.post('https://api.nexmo.com/v0.3/users', body,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + accessToken
+                }
+            });
+        console.log("User creation results: ", results.data);
+        
+        // New user created, pass back the id
+        resolve(results.data.id);
+        
+        return;
+    } 
+    catch (err) {
+        console.log(">>> User creation error:", err);
+        console.log("User creation error: ", err.response?.statusText)
+        
+        resolve(null);
+    }
+  })
+
+}
+
+//--------
+
+app.post('/logout', async (req, res) => {
+    
+    let user = req.body.user;
+    let session = req.body.session;
+    
+    // console.log("Deleting session: " + session);
+    await delSession(session);
+
+    return res.status(200).end();
+})
+
+//--------
+
+async function generateJWT(sub) {
+    
+    // Generate a JWT with the appropriate ACL
+    let jwtExpiration = Math.round(new Date().getTime() / 1000) + 2592000; //30 days
+    
+    const aclPaths = {
+        "paths": {
+            "/*/users/**": {},
+            "/*/conversations/**": {},
+            "/*/sessions/**": {},
+            "/*/devices/**": {},
+            "/*/image/**": {},
+            "/*/media/**": {},
+            "/*/applications/**": {},
+            "/*/push/**": {},
+            "/*/knocking/**": {},
+            "/*/legs/**": {}
+        }
+    }
+    let claims = {
+        exp: jwtExpiration,
+        //ttl: 86400,
+        acl: aclPaths,
+    }
+    
+    // ONLY Client JWTs use a "sub", so don't add one if it is already passed in
+    if (sub != null) {
+        claims.sub = sub
+    }
+    
+    //-- debug
+    // console.log(appId, privateKey, claims);
+    
+    const jwt = tokenGenerate(appId, privateKey, claims)
+    
+    // debug
+    // console.log("Jwt: ", jwt)
+    
+    return (jwt);
+}
+
+//--------
+
+async function delSession(session) {
+
+  // to do, fix the issue where it tries to delete again past deleted sessions
+
+  const accessToken = tokenGenerate(appId, privateKey, {});
+  
+  return new Promise(async (resolve, reject) => {
+
+    let results;
+
+    try {
+      results = await axios.delete('https://api.nexmo.com/v0.3/sessions/' + session,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+          }
+        });
+      // console.log("User session deletion results: ", results.data);
+      resolve(results.data);
+      return;
+    } 
+    catch (err) {
+      // console.log("User session deletion error: ", err)
+    }
+  })
+
+}
+
+//--------------- for VCR ----------------
+
+app.get('/_/health', async (req, res) => {
+   
+  res.status(200).send('Ok');
+
+});
+
+//========== Static HTTP server ===========
+
+app.use ('/', express.static(__dirname + '/public')); // static web server
+
+//=========================================
+
+const port = process.env.NERU_APP_PORT || process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Application listening on port ${port}`));
+
+//------------
